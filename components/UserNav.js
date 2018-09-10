3f8a8b349cc701803095ac2db9d92e00143748f0
Grizvok <burninglovedesign@gmail.com>
2018-09-10 11:05:41 -0400

    UI work

--- components/UserNav.js
+++ components/UserNav.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 class UserNav extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    console.log(this.props);
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            <img
-              src="https://bulma.io/images/bulma-logo.png"
-              alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </a>
-
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </a>
-        </div>
-      </nav>
+      <div className="tabs has-background-white is-centered is-boxed">
+        <ul>
+          <li className="is-active">
+            <Link prefetch href="">
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-book-open" aria-hidden="true" />
+                </span>
+                <span>Overview</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-receipt" aria-hidden="true" />
+                </span>
+                <span>Tickets</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link>
+              <a>
+                <span className="icon is-small">
+                  <i className="fas fa-film" aria-hidden="true" />
+                </span>
+                <span>Skills</span>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
